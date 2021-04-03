7ce5836e2e53eebf341a612341ce25a3739680e9
YarKirillof <76162477+YarKirillof@users.noreply.github.com>
2021-04-03 19:58:30 +0300

    WinTimer2

---
+++ WinTimer2/WinTimer2/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WinTimer2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControl21 = new WinTimer2.UserControl2();
+            this.SuspendLayout();
+            // 
+            // userControl21
+            // 
+            this.userControl21.Location = new System.Drawing.Point(30, 27);
+            this.userControl21.Name = "userControl21";
+            this.userControl21.Size = new System.Drawing.Size(75, 23);
+            this.userControl21.TabIndex = 0;
+            this.userControl21.Text = "userControl21";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.userControl21);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControl2 userControl21;
+    }
+}
+
