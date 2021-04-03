234c30cb60f8f1451cd3884e51812209cb9138a4
YarKirillof <76162477+YarKirillof@users.noreply.github.com>
2021-04-03 20:15:22 +0300

    MdiApplication

---
+++ MdiApplication/MdiApplication/ChildForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace MdiApplication
+{
+    partial class ChildForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChildTextBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ChildTextBox
+            // 
+            this.ChildTextBox.BackColor = System.Drawing.Color.Green;
+            this.ChildTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ChildTextBox.Location = new System.Drawing.Point(0, 0);
+            this.ChildTextBox.Name = "ChildTextBox";
+            this.ChildTextBox.Size = new System.Drawing.Size(800, 450);
+            this.ChildTextBox.TabIndex = 1;
+            this.ChildTextBox.Text = "";
+            this.ChildTextBox.TextChanged += new System.EventHandler(this.ChildTextBox_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(321, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GREENPEACE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // ChildForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ChildTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ChildForm";
+            this.Text = " Child Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox ChildTextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ MdiApplication/MdiApplication/ChildForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MdiApplication
+{
+    public partial class ChildForm : Form
+    {
+        public ChildForm()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ChildTextBox_TextChanged(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath =
+                new System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddPolygon(new Point[] { new Point(0, 0),
+                new Point(0, this.Height),
+                new Point(this.Width, 0) });
+            Region myRegion = new Region(myPath);
+            this.Region = myRegion;
+        }
+    }
+}
